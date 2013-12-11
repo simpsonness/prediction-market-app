5a5fc30b8d921133a936ac5e2e1eb0e53bcb60c3
shan <corallishan@gmail.com>
2013-12-11 12:19:35 -0500

    Added update destory and edit to event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -24,13 +24,29 @@ class EventsController < ApplicationController
   end
 
   def edit
-    @event = Event.find(event_params)
+    @event = Event.find(params[:id])
   end
-   
+
+  def update
+    @event = Event.find(params[:id])
+
+    if @event.update_attributes(event_params)
+      redirect_to event_path(@event)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to events_path
+  end
+
   private
 
   def event_params
-    params.require(:event).permit(:title, :description)
+    params.require(:event).permit(:title, :description, :price)
   end
 
 end
