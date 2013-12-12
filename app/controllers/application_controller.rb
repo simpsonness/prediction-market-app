0d2f64d6d93ddacd3b31ac0fb4e781ead5aae92b
shan <corallishan@gmail.com>
2013-12-12 17:37:13 -0500

    add before_filter to eventscontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def not_athenticated
+  def not_authenticated
     redirect_to login_url, :alert => "First login to access this page."
   end
 end
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,5 @@
 class EventsController < ApplicationController
+  before_filter :require_login
   def index
     @events = Event.all
   end
