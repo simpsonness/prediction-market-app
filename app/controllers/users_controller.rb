8e97289ce942e3ed6a9cf613d69025d7f4520527
shan <corallishan@gmail.com>
2013-12-13 16:32:11 -0500

    dropped options -> orders

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @user = current_user
+    
   end
 
   def index
--- db/migrate/20131212192057_rename_option_to_order.rb
+++
@@ -1,5 +0,0 @@
-class RenameOptionsToOrders < ActiveRecord::Migration
-  def change
-    rename_table :options, :orders
-  end
-end
